c53aaa7227ee3c4bb24f476c8fd5f23958c852f5
risper4 <risperwanjikugichia@gmail.com>
2026-02-12 13:09:33 +0300

    Added final js

--- address.js
+++ address.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 function Car(){
     this.id = new Date;
     this.name = name;
@@ -48,7 +47,7 @@ document.getElementById("form").addEventListener('submit', (event) => {
 let s = document.getElementById("visitedplaces");
 let car1 = new Car(s.value);
 garage1.add;
-=======
+
 function Car(){
     this.id = new Date;
     this.name = name;
@@ -97,54 +96,4 @@ document.getElementById("form").addEventListener('submit', (event) => {
 let s = document.getElementById("visitedplaces");
 let car1 = new Car(s.value);
 garage1.add;
->>>>>>> ed2d41aceb752898b46ab2522b8af5cb5ad74ffb
-=======
-function Car(){
-    this.id = new Date;
-    this.name = name;
-}
 
-function Garage(){
-    this.n = new Map();
-}
-
-Garage.prototype.add = (d) => {
-    this.p.set(d.id, d);
-    this.r();
-}
-
-Garage.prototype.delete = (id) => {
-    this.p.delete(id);
-    this.r();
-}
-
-Garage.prototype.update = (id) => {
-    this.p.get(id);
-    this.r();
-}
-
-Garage.prototype.read = (id) => {
-    let ul = document.getElementById("list");
-    ul.innerHTML = ``
-
-    this.p.forEach =function(j){
-        let li = document.createElement(li);
-        li.database.id = j.id;
-        li.innerHTML = `
-        <strong><em>${j.name}</em></strong>
-        `;
-        ul.appendChild.li;
-    }
-}
-
-let garage1 = new Garage();
-
-document.getElementById("form").addEventListener('submit', (event) => {
-    event.preventDefault();
-    alert('Task submitted')
-})
-
-let s = document.getElementById("visitedplaces");
-let car1 = new Car(s.value);
-garage1.add;
->>>>>>> ed2d41aceb752898b46ab2522b8af5cb5ad74ffb
